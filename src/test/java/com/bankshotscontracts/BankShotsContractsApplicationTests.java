1895f4b8fcc16a62b1eb037b3cf533fd0557224a
Dominique Jones <developerdomjones@gmail.com>
2020-02-20 21:26:26 -0500

    initial commit

---
+++ src/test/java/com/bankshotscontracts/BankShotsContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankshotscontracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankShotsContractsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
