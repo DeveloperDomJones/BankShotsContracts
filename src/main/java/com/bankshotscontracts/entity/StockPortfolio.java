a56a210d049fa94221823fb1b315f47cfa48b600
Dominique Jones <developerdomjones@gmail.com>
2020-04-08 16:20:18 -0400

    Added account id and id to stock portfolio entity

--- src/main/java/com/bankshotscontracts/entity/StockPortfolio.java
+++ src/main/java/com/bankshotscontracts/entity/StockPortfolio.java
@@ -1,5 +1,7 @@
 package com.bankshotscontracts.entity;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Getter;
@@ -10,7 +12,10 @@ import lombok.Setter;
 @Document(collection = "stockPortfolio")
 public class StockPortfolio {
 	
+	@Id
+	private ObjectId id;
 	private String symbol;
 	private double holdings;
+	private String accountId;
 
 }
